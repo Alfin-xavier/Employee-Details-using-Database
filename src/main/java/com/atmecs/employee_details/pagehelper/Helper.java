df6bd19e06042832cb9616e4c1d851df1e9bc629
ATMECS\Alfin.Anthonyraj <alfin.anthonyraj@atmecs.com>
2020-10-16 13:54:49 +0530

    Updated

---
+++ src/main/java/com/atmecs/employee_details/pagehelper/Helper.java
@@ -0,0 +1,16 @@
+package com.atmecs.employee_details.pagehelper;
+
+import java.io.IOException;
+import java.util.Properties;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import com.atmecs.employee_details.constants.Constants;
+import com.atmecs.employee_details.helpers.HelperClass;
+import com.atmecs.employee_details.utilities.Logging;
+import com.atmecs.employee_details.utilities.PropertyReader;
+
+public class Helper 
+{
+	
+	
+}
