642d36b32048ab66bf0a88f57cd3b741bd71e045
ATMECS\Alfin.Anthonyraj <alfin.anthonyraj@atmecs.com>
2020-10-19 10:31:21 +0530

    Updated

--- src/main/java/com/atmecs/employee_details/pagehelper/Helper.java
+++ src/main/java/com/atmecs/employee_details/pagehelper/Helper.java
@@ -5,33 +5,49 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Properties;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import com.atmecs.employee_details.constants.Constants;
+import com.atmecs.employee_details.utilities.PropertyReader;
 
 public class Helper 
 {
 	public void getFromDb() throws SQLException, ClassNotFoundException
 	{
+		WebDriver driver;
+		Properties properties;
+		String baseUrl;
+		String browserUrl;
+
 		String dbUrl = "jdbc:mysql://127.0.0.1:3306/employee";
-		 String username = "Alfin Anthonyraj";
-		 String password = "AlfinXavier@118";
-		 String query = "select *  from emp_details;";
-		 Class.forName("com.mysql.cj.jdbc.Driver");
-		 Connection con = DriverManager.getConnection(dbUrl,username,password);
-		 Statement stmt = con.createStatement();
-		 ResultSet rs= stmt.executeQuery(query);
+		String username = "Alfin Anthonyraj";
+		String password = "AlfinXavier@118";
+		String query = "select *  from emp_details;";
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		Connection con = DriverManager.getConnection(dbUrl, username, password);
+		Statement stmt = con.createStatement();
+		ResultSet rs = stmt.executeQuery(query);
+		
 		 while (rs.next())
 		 {
-			int id = rs.getInt(1);
-    		String name = rs.getString(2);								        
-    		String email = rs.getString(3); 
-    		String phone = rs.getString(4);
-    		
-    		System.out.println("Employee Details\n");
-            System. out.println("Employee Id:"+" "+id);
-            System.out.println("Employee Name:"+" "+name);
-            System.out.println("Employee Mail:"+" "+email);
-            System.out.println("Mobile:"+" "+phone);
+			 properties = PropertyReader.readProperties(Constants.CONFIG);
+				baseUrl = properties.getProperty("url");
+				browserUrl = properties.getProperty("browser");
+				
+					System.setProperty(Constants.CHROME_DIR, Constants.CHROME_PATH);
+					driver = new ChromeDriver();
+					driver.get(baseUrl);
+			 
+			 driver.findElement(By.xpath("//input[@id='email']")).sendKeys(rs.getString("e_mail"));
+			 driver.findElement(By.xpath("//input[@id='pass']")).sendKeys(rs.getString("emp_name"));
+			 driver.findElement(By.xpath("//button[@name='login']")).click();
 		 }
         con.close();
+        
 
 	}
 	
--- src/test/java/com/atmecs/employee_details/tests/Employee_Datails.java
+++ src/test/java/com/atmecs/employee_details/tests/Employee_Datails.java
@@ -3,10 +3,9 @@ package com.atmecs.employee_details.tests;
 import java.sql.SQLException;
 
 import org.testng.annotations.Test;
-
 import com.atmecs.employee_details.pagehelper.Helper;
 
-public class Employee_Datails 
+public class Employee_Datails
 {
 	@Test
 	public void getData() throws SQLException, ClassNotFoundException
